8e6d0c5b6d5dcd5a9c2f97c74d61be33ca8c4696
A'ir <syrill@yandex.ru>
2022-06-08 17:45:25 +0300

    Создана кастомная модель пользователя

--- tournaments/settings.py
+++ tournaments/settings.py
@@ -82,6 +82,8 @@ DATABASES = {
 }
 
 
+AUTH_USER_MODEL = 'users.CustomUser'
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,56 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import UserManager, PermissionsMixin
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _('A user with that username already exists.')
+        }
+    )
+
+    email = models.EmailField(_('email address'), unique=True)
+
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        )
+    )
+
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_(
+            'Designates whether the user can log into this admin site.'
+        )
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        db_table = 'users_user'
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
