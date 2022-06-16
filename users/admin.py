e2ad38e463aa57220ef9be8a0cd38d669ad89401
A'ir <syrill@yandex.ru>
2022-06-16 12:35:45 +0300

    Кастомная модель пользователя добавлена в админку

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,82 @@
-from django.contrib import admin
 
-# Register your models here.
+from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.utils.translation import gettext_lazy as _
+
+from users.models import CustomUser
+
+
+
+class UserCreationForm (forms.ModelForm):
+
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+
+class UserChangeForm (forms.ModelForm):
+
+    password = ReadOnlyPasswordHashField(
+        help_text=_(
+            "Raw passwords are not stored, so there is no way to see this "
+            "user’s password, but you can change the password using "
+            '<a href="../password/">this form</a>.'
+        ),
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password', 'date_joined', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+
+
+
+class UserAdmin (BaseUserAdmin):
+
+    add_form = UserCreationForm
+    form = UserChangeForm
+
+    list_display = ('username', 'email', 'date_joined', 'last_login', 'is_staff')
+    list_filter = ('is_staff',)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password', 'date_joined')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide'),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+
+    search_fields = ('username',)
+    ordering = ('username',)
+    filter_horizontal = ()
+
+
+
+admin.site.register(CustomUser, UserAdmin)
