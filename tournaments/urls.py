09e9b98ae61c04b7e486aa91555dd0f5c27fe31a
A'ir <syrill@yandex.ru>
2022-06-18 16:14:21 +0300

    Добавлена возможность логина и логаута

--- tournaments/urls.py
+++ tournaments/urls.py
@@ -1,6 +1,6 @@
 
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
 
 from . import views
 
@@ -8,5 +8,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage)
+    path('users/', include('users.urls')),
+    path('', views.homepage, name='homepage')
 ]
