0b58305c3a9c66ed9e3946bf5ab928ad891458f1
A'ir <syrill@yandex.ru>
2022-08-13 18:45:18 +0300

    добавлен inclusion tag, выводящий отдельный отчет

--- reports/templatetags/reports_extras.py
+++ reports/templatetags/reports_extras.py
@@ -6,6 +6,16 @@ from django import template
 register = template.Library()
 
 
+
 @register.filter
 def getkey(dictionary, key):
     return dictionary.get(key, '')
+
+
+
+@register.inclusion_tag('reports/report.html', takes_context=True)
+def report(context):
+    return {
+        'request': context['request'],
+        'report': context['report'],
+    }
